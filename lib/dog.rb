f619daa90fc1fcc36596ac797f73c1a3757171be
Jose Soto <jsoto7087@gmail.com>
2021-02-18 10:06:20 -0500

    commit tree fdde3f69866d4cd03cbe64c2258f79a11b9f7bda

---
+++ lib/dog.rb
@@ -0,0 +1,56 @@
+# class Dog
+
+#     def name=(name)
+#         @name = name
+       
+#     end
+    
+#     def name
+#         @name
+     
+#     end
+
+#     def breed=(breed)
+#         @breed = breed
+      
+
+#     end
+
+#     def breed
+#         @breed
+#     end
+
+# end
+
+
+class Dog
+    attr_accessor :name, :breed
+
+    # def initialize(name, breed)
+    #     @name = name
+    #     @breed = breed
+    # end 
+
+    def name=(name)
+        @name = name
+    end 
+
+    def name 
+        @name 
+    end 
+    
+    def breed=(breed)
+        @breed = breed
+    end 
+
+    def breed
+        @breed 
+    end 
+
+end 
+
+
+
+
+
+
---
+++ lib/person.rb
@@ -0,0 +1,61 @@
+# class Person
+
+#     def name=(name)
+#       @name = name
+#     end
+#     def name
+#       @name
+#     end
+  
+#     def job=(job)
+#       @job = job
+#     end
+  
+#     def job
+#       @job
+#     end
+# end
+
+class Person 
+
+  attr_accessor :name, :job
+
+  # def initialize(first_name, last_name)
+  #   @first_name = first_name
+  #   @last_name = last_name
+  # end 
+
+  # def name=(name)
+  #   @name = name
+  # end 
+
+  # def name
+  #   @name
+  # end 
+
+  # def job=(job)
+  #   @job = job
+  # end 
+
+  # def job 
+  #   @job 
+  # end 
+
+end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
